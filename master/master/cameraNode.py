74858d3d9d29b00293c6f84bf140f5590948789b
pettras <petteras@gmail.com>
2022-05-24 14:22:21 +0200

    Code for 3d capture. Ludvig master thesis.

--- master/master/cameraNode.py
+++ master/master/cameraNode.py
@@ -3,16 +3,53 @@ import sys
 from zivid_interfaces.srv import Capture
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import Image
 
 
-class MinimalClientAsync(Node):
+class PublishingSubscriber(Node):
 
     def __init__(self):
         super().__init__('camera_node')
+
+        self.subscription_camera = self.create_subscription(
+            Image,
+            '/zivid/depth/image',
+            self.zivid_image_callback,
+            1)
+        #self.current_image = np.zeros([1200,1944,4], dtype=np.uint8)
+
+        timer_period = 5  # seconds
+        self.timer = self.create_timer(
+            timer_period, 
+        self.capture3d) #sends gripper pos accorting to timer
+
         self.cli = self.create_client(Capture, 'zivid/capture')
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
         self.req = Capture.Request()
+
+    def capture3d(self):
+        print("Capturing")
+        node = rclpy.create_node("zivid_camera_client")
+
+        capture_client = node.create_client(Capture, "/zivid/capture")
+        while not capture_client.wait_for_service(timeout_sec=1.0):
+            print("service not available, waiting again...")
+
+        request = Capture.Request()
+        future = capture_client.call_async(request)
+        node.destroy_node()
+        rclpy.shutdown()
+
+    def zivid_image_callback(self, msg):
+        self.current_image = msg.data
+        print("Recieved image")
+        self.use_image()
+
+    def use_image(self):
+        image_state = self.current_image
+        print("Using image")
+
 
     def send_request(self):
         self.req 
@@ -21,27 +58,19 @@ class MinimalClientAsync(Node):
         #self.future = self.cli.call_async(self.req)
 
 
-def main():
-    rclpy.init()
 
-    minimal_client = MinimalClientAsync()
-    minimal_client.send_request()
-    print("ja")
-    while rclpy.ok():
-        rclpy.spin_once(minimal_client)
-        if minimal_client.future.done():
-            try:
-                response = minimal_client.future.result()
-            except Exception as e:
-                minimal_client.get_logger().info(
-                    'Service call failed %r' % (e,))
-            else:
-                minimal_client.get_logger().info(
-                    'Result of add_two_ints: for %d + %d = %d' %
-                    (minimal_client.req.a, minimal_client.req.b, response.sum))
-            break
+def main(args=None):
 
-    minimal_client.destroy_node()
+    rclpy.init(args=args)
+
+    publisher_and_subscriber = PublishingSubscriber()
+
+    rclpy.spin(publisher_and_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    publisher_and_subscriber.destroy_node()
     rclpy.shutdown()
 
 
